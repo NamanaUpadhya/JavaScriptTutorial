39e6695a766863eb9a9110b8275ddd705e0db3ab
Namana Upadhya <namanaupadhya2@gmail.com>
2024-08-02 12:32:27 +0530

    newadd

--- First.js
+++ First.js
@@ -1,3 +1,55 @@
-fullName=prompt("Enter Full Name:");
-userName=`@${fullName}${fullName.length}`
-console.log(userName);
+// let fullName=prompt("Enter Full Name:");
+// let userName=`@${fullName}${fullName.length}`
+// console.log(userName);
+
+// const arrowMul = (a,b)=>{
+//     return a*b;
+// };
+// function countVowels(str){
+//     let count = 0;
+//     for(const char of str ){
+//         if(
+//            char === "a" ||
+//            char === "e" ||
+//            char === "i" ||
+//            char === "o" ||
+//            char === "u"
+//         ){
+//             count++;
+//         }
+//     }
+//     console.log(count);
+// }
+
+// arr=["pune","mumbai","udupi","mangalore"];
+// arr.forEach((val, idx, arr) => {
+//     console.log(val.toUpperCase(), idx, arr);
+// });
+
+// arr=[1,2,3,4,5,6,78,9009,23335,2222];
+// forEach
+// arr.forEach((val)=>{
+//     console.log(val*val);
+// })
+
+
+// Map
+// let newArr = arr.map((val)=>{
+//     return(val*4);
+// });
+// console.log(newArr);
+
+
+// Filter
+// let newArr=arr.filter((val)=>{
+//     return val%2==0;
+// })
+// console.log(newArr);
+
+
+const array1=[1,2,3,4];
+
+const output = array1.reduce((res,cur)=>{
+    return res+cur;
+})
+console.log(output);
