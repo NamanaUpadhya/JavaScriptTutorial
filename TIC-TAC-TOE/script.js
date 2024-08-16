bcf3f47cb82f57c50152345afc5d6699e96c64ea
Namana Upadhya <namanaupadhya2@gmail.com>
2024-08-16 15:11:17 +0530

    TIC_TAC_TOE

--- TIC-TAC-TOE/script.js
+++ TIC-TAC-TOE/script.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#resetBtn");
+let newBtn = document.querySelector("#newBtn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turnO = true;//playerX , playerO
 
@@ -13,6 +16,13 @@ const winPatterns = [
     [3,4,5],
     [6,7,8]
 ]
+
+const resetGame = ()=>{
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
 
 boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
@@ -29,6 +39,45 @@ boxes.forEach((box)=>{
     });
 });
 
+const disableBoxes = () =>{
+    for(let box of boxes){
+        box.disabled = true;
+    }
+};
+
+const enableBoxes = () =>{
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    }
+};
+
+const showWinner = (Winner) =>{
+    msg.innerText = `Congratualtions, Winner is ${Winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+
 const checkWinner = ()=>{
-    
-}
+    for (let pattern of winPatterns){
+        // console.log(pattern[0],pattern[1],pattern[2]);
+        // console.log(
+        //     boxes[pattern[0]].innerText,
+        //     boxes[pattern[1]].innerText,
+        //     boxes[pattern[2]].innerText
+        // );
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+        if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
+            if(pos1Val === pos2Val && pos2Val=== pos3Val){
+                console.log("Winner!!",pos1Val);
+                showWinner(pos1Val);
+            }
+        }
+    }
+};
+
+newBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
